7577bdb5b461a201736e4ae43d252d6a8d347b43
zcode25 <adamzein345@gmail.com>
2022-02-13 13:33:14 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, repudiandae quam cupiditate, quibusdam expedita deserunt sint, nemo ab sunt eligendi velit maxime eaque hic assumenda incidunt aut eos cum magnam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
